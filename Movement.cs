c23ac4543fd44549f0918b38069a39090d722668
k1llxna <44785075+k1llxna@users.noreply.github.com>
2019-01-25 00:04:35 -0500

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public enum DIRECTION { UP, DOWN, LEFT, RIGHT}
+
+public class Movement : MonoBehaviour {
+
+    public Sprite south;
+    public Sprite north;
+    public Sprite east;
+    public Sprite west;
+
+    public Transform rCol;
+    public Transform lCol;
+    public Transform uCol;
+    public Transform dCol;
+
+    private bool canMove = true, moving = false;
+    private int speed = 4, buttonCooldown = 0;
+    private DIRECTION dir = DIRECTION.DOWN;
+    private Vector3 pos;
+
+    private void FixedUpdate()
+    {
+      
+    }
+    
+	void Start () {	}
+	
+	// Update is called once per frame
+	void Update () {
+        buttonCooldown--;
+        if (canMove)
+        {
+            pos = transform.position;
+            Move();
+        }
+
+        if (moving)
+        {
+            if (transform.position == pos)
+            {
+                moving = false;
+                canMove = true;
+
+                Move();
+            }
+            transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+        }
+	}
+
+    private void Move()
+    {
+        if (buttonCooldown <= 0) {
+            if (Input.GetKey(KeyCode.W))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(uCol.position, Vector2.up, 1.0f);
+                if (hit) 
+                {
+                    Debug.Log("Collided with " + hit.transform.name);
+                }
+                else
+                {
+                    if (dir != DIRECTION.UP)
+                    {
+                        buttonCooldown = 5;
+                        dir = DIRECTION.UP;
+                    }
+                    else
+                    {
+                        canMove = false;
+                        moving = true;
+                        pos += Vector3.up;
+                        gameObject.GetComponent<SpriteRenderer>().sprite = north;
+                    }
+                }         
+            }
+
+           else if (Input.GetKey(KeyCode.S)) {
+                RaycastHit2D hit = Physics2D.Raycast(dCol.position, Vector2.down, 1.0f);
+                if (hit) 
+                {
+                    Debug.Log("Collided with " + hit.transform.name);
+                }
+                else
+                {
+                    if (dir != DIRECTION.DOWN)
+                    {
+                        buttonCooldown = 5;
+                        dir = DIRECTION.DOWN;
+                    }
+                    else
+                    {
+                        canMove = false;
+                        moving = true;
+                        pos += Vector3.down;
+                        gameObject.GetComponent<SpriteRenderer>().sprite = south;
+                    }
+                }  
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(lCol.position, -Vector2.right, 1.0f);
+                if (hit) 
+                {
+                    Debug.Log("Collided with " + hit.transform.name);
+                }
+                else
+                {
+                    if (dir != DIRECTION.LEFT)
+                    {
+                        buttonCooldown = 5;
+                        dir = DIRECTION.LEFT;
+                    }
+                    else
+                    {
+                        canMove = false;
+                        moving = true;
+                        pos += Vector3.left;
+                        gameObject.GetComponent<SpriteRenderer>().sprite = west;
+                    }
+                }          
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                RaycastHit2D hit = Physics2D.Raycast(rCol.position, Vector2.right, 1.0f);
+                if (hit) 
+                {
+                    Debug.Log("Collided with " + hit.transform.name);
+                }
+                else
+                {
+                    if (dir != DIRECTION.RIGHT)
+                    {
+                        buttonCooldown = 5;
+                        dir = DIRECTION.RIGHT;
+                    }
+                    else
+                    {
+                        canMove = false;
+                        moving = true;
+                        pos += Vector3.right;
+                        gameObject.GetComponent<SpriteRenderer>().sprite = east;
+                    }
+                }
+               
+            }
+        }
+    }
+}
